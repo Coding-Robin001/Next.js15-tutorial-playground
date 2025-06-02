598954bf61a19ef0850db2d4c5dbe2d6a45255b8
Coding-Robin001 <codingade@gmail.com>
2025-06-02 17:53:25 +0100

    commit

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -8,8 +8,11 @@ const gl = () => {
             <body>
                 <div className='flex justify-center items-center h-[100vh]'>
                     <div className='flex flex-col gap-4 justify-center items-center'>
-                        <h2 className='text-[2rem]'>Something went wrong!
+                        <h2 className='text-[3rem]'>Something went wrong!
                         </h2>
+                        <h2 className='text-center text-[brown] text-[1.3rem]'>this is a global-error.tsx component that kicks in as an error boundary for our entire app
+                        </h2>
+                        <span className='text-center text-[green] text-[1.1rem]'>act as our last line of defence so our app dont crash</span>
                         <div>
                             <button
                                 onClick={() => {
