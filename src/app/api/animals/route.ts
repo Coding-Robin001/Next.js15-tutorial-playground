b98c9123ce243aa680671fffb40c6b1bdf3f2aab
Coding-Robin001 <codingade@gmail.com>
2025-07-08 15:14:15 +0100

    prisma migration to sqlite table

--- src/app/api/animals/route.ts
+++ src/app/api/animals/route.ts
@@ -2,11 +2,13 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
+// GET all animals
 export async function GET() {
   const animals = await prisma.animal.findMany()
   return Response.json(animals)
 }
 
+// CREATE new animal
 export async function POST(req: Request) {
   const body = await req.json()
   const newAnimal = await prisma.animal.create({
@@ -16,5 +18,5 @@ export async function POST(req: Request) {
       fact: body.fact,
     }
   })
-  return Response.json(newAnimal)
+  return Response.json(newAnimal, { status: 201 })
 }
