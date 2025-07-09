0c1f402e1aee06f84805771ae461405f796c4dc9
Coding-Robin001 <codingade@gmail.com>
2025-07-09 16:22:42 +0100

    commit

--- src/app/photo-feed/@modal/(.)[photoId]/page.tsx
+++ src/app/photo-feed/@modal/(.)[photoId]/page.tsx
@@ -17,19 +17,21 @@ const PhotoFeedModal = ({
   return (
     <div className='modal-container'>
       <div className='modal-wrapper'>
-        <div className=' h-[300px] flex items-center justify-center'
+        <div className=' h-[320px] flex items-center justify-center'
           style={{
             borderRadius: "3px",
             border:"1px solid #353232"
           }}>
-          <Image
-            className='object-contain w-[450px] h-[300px]'
-            src={currentPhoto?.src}
-            alt={currentPhoto?.name}
+         <Image
+            className="object-contain w-[450px] h-[300px]"
+            src={currentPhoto.src}
+            alt={`Photo of ${currentPhoto.name}`}
+            width={450}
+            height={300}
           />
         </div>
         <div className='w-[500px] '>
-          <h2 className='text-[#fff] text-[1.9rem]'>{currentPhoto?.name.toUpperCase()}</h2>
+          <h2 className='text-[#fff] text-[1.6rem] font-bold text-orange-300'>{currentPhoto?.name.toUpperCase()}</h2>
           <p className='text-[#fff] '><span className='text-[chocolate]'>FUNFACT:</span> {currentPhoto?.randomFact}</p>
         </div>
       </div>
