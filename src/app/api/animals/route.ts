3aee91cd1e98f91bd95e1cb91e770c699cf7bee8
Coding-Robin001 <codingade@gmail.com>
2025-07-13 17:36:38 +0100

    refactor post request with route handlers to include valifdations and error-handling

--- src/app/api/animals/route.ts
+++ src/app/api/animals/route.ts
@@ -4,19 +4,33 @@ const prisma = new PrismaClient()
 
 // GET all animals
 export async function GET() {
-  const animals = await prisma.animal.findMany()
-  return Response.json(animals)
+  try {
+    const animals = await prisma.animal.findMany()
+    return Response.json(animals)
+  } catch (error) {
+    console.error(error)
+    return Response.json({ error: 'Failed to fetch animals' }, { status: 500 })
+  }
 }
 
-// CREATE new animal
+
 export async function POST(req: Request) {
   const body = await req.json()
-  const newAnimal = await prisma.animal.create({
-    data: {
-      name: body.name,
-      photoUrl: body.photoUrl,
-      fact: body.fact,
-    }
-  })
-  return Response.json(newAnimal, { status: 201 })
+  if (!body.name || !body.photoUrl || !body.fact) {
+    return Response.json({ error: 'Missing required fields' }, { status: 400 });
+  }
+
+  try {
+    const newAnimal = await prisma.animal.create({
+      data: {
+        name: body.name,
+        photoUrl: body.photoUrl,
+        fact: body.fact,
+      }
+    })
+    return Response.json(newAnimal, { status: 201 })
+  } catch (error) {
+    console.error(error)
+    return Response.json({ error: 'Failed to create animal, something went wrong!' }, { status: 500 })
+  }
 }
