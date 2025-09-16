fbaf9a497158a506fd314d25bcc7f820be344637
Coding-Robin001 <codingade@gmail.com>
2025-09-16 14:52:55 +0100

    errorHandling+validation with useActionState+Server actions in Next.js15

---
+++ src/actions'/animals.ts
@@ -0,0 +1,41 @@
+"use server"
+
+import { addAnimal } from "@/prisma-db";
+import { redirect } from "next/navigation";
+
+
+export type Errors = {
+    name?: string,
+    photoUrl?: string,
+    fact?: string
+}
+
+export type FormState = {
+    errors: Errors;
+}
+
+export async function createAnimal(prevState: FormState, formdata: FormData) {
+
+    const name = formdata.get("name") as string
+    const photoUrl = formdata.get("photoUrl") as string
+    const fact = formdata.get("fact") as string
+
+    const errors: Errors = {}
+
+    if (!name) {
+        errors.name = "title is required!"
+    }
+    if (!photoUrl) {
+        errors.photoUrl = "photoUrl is required!"
+    }
+    if (!fact) {
+        errors.fact = "fact is required!"
+    }
+
+    if (Object.keys(errors).length > 0) {
+        return { errors: errors }
+    }
+
+    await addAnimal(name, photoUrl, fact)
+    redirect("/animals-db")
+}
--- src/app/animals-db-create/page.tsx
+++ src/app/animals-db-create/page.tsx
@@ -1,17 +1,9 @@
-import { addAnimal } from "@/prisma-db"
+"use client"
+
 import Submit from "@/components/submit"
-import { redirect } from "next/navigation"
 import { useActionState } from "react"
 
-type Errors = {
-    name?: string,
-    photoUrl?: string,
-    fact?: string
-}
-
-type FormState = {
-    errors: Errors;
-}
+import { FormState, createAnimal } from "@/actions'/animals"
 
 export default function AddAnimalForm() {
 
@@ -21,37 +13,11 @@ export default function AddAnimalForm() {
 
     const [state, formAction, isPending] = useActionState(createAnimal, initialState)
 
-    async function createAnimal(formdata: FormData) {
-        "use server"
-
-        const name = formdata.get("name") as string
-        const photoUrl = formdata.get("photoUrl") as string
-        const fact = formdata.get("fact") as string
-
-        const errors: Errors = {}
-
-        if (!name) {
-            errors.name = "title is required!"
-        }
-        if (!photoUrl) {
-            errors.photoUrl = "photoUrl is required!"
-        }
-        if (!fact) {
-            errors.fact = "fact is required!"
-        }
-
-        if (Object.keys(errors).length > 0) {
-            return { errors: errors }
-        }
-
-        await addAnimal(name, photoUrl, fact)
-        redirect("/animals-db")
-    }
 
     return (
         <form
             action={formAction}
-            className="max-w-lg border-green-300 mx-auto p-6 bg-green-50 rounded-2xl shadow-lg space-y-8 border border-green-300"
+            className="max-w-lg w-[800px] border-green-300 mx-auto p-6 bg-green-50 rounded-2xl shadow-lg space-y-8 border border-green-300"
         >
             <h2 className="text-2xl font-semibold text-center text-green-800">
                 Add New Animal
@@ -63,7 +29,6 @@ export default function AddAnimalForm() {
                     placeholder="Animal Name"
                     className="w-full p-2 border border-green-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
                     name="name"
-                    required
                 />
                 {
                     state.errors.name && <p className="text-red-500">{state.errors.name}</p>
@@ -76,7 +41,7 @@ export default function AddAnimalForm() {
                     placeholder="Photo URL"
                     className="w-full p-2 border border-green-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
                     name="photoUrl"
-                    required
+                    // required
                 />
                 {
                     state.errors.photoUrl && <p className="text-red-500">{state.errors.photoUrl}</p>
@@ -87,7 +52,7 @@ export default function AddAnimalForm() {
                 <textarea
                     placeholder="Random Fact"
                     className="w-full p-2 border border-green-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
-                    required
+                    // required
                     name="fact"
                 />
                 {
